fc1c7c705956d1a06c6556f4b5c57d171dce492d
a-khomitskyi <homitskiy.andriy@gmail.com>
2021-11-06 15:50:36 +0200

    upd

--- client.py
+++ client.py
@@ -17,11 +17,11 @@ async def tcp_echo_client(message):
     reader, writer = await asyncio.open_connection(
         '127.0.0.1', 8888)
 
-    print(f'Send: {message!r}')
+    print(f'Send: {message}')
     writer.write(message.encode())
 
     data = await reader.read(100)
-    print(f'Received: {data.decode()!r}')
+    print(f'Received: {data.decode()}')
 
     print('Close the connection')
     writer.close()
